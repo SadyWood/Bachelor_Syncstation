ccc6e9bb7bbe469e541f8e319ad7478cbda53310
Nina <ninafatahii@gmail.com>
2026-02-23 03:04:05 +0100

    Feat: Added Animation for circle with fade in

--- apps/syncstation-app/src/screens/login-screen/LoginScreen.tsx
+++ apps/syncstation-app/src/screens/login-screen/LoginScreen.tsx
@@ -12,6 +12,7 @@ import {
 } from 'react-native';
 import { styles } from './LoginScreen.styles';
 import { useAuthStore } from "@/stores/authStore";
+import {multiply} from "react-native/Libraries/Animated/AnimatedExports";
 
 const { width, height } = Dimensions.get('window');
 
@@ -20,3 +21,40 @@ const BUBBLES = [
   { top: 0.09, left: 0.38, size: 0.40, delay: 100 },
   { top: 0.24, left: 0.78, size: 0.20, delay: 200 },
 ];
+
+type CircleProps = {
+  top: number,
+  left: number,
+  size: number,
+  delay: number,
+};
+
+function AnimatedCircle({ top, left, size, delay }: CircleProps) {
+  const fadeAnimation = useRef(new Animated.Value(0)).current;
+  const diameter = width * size;
+
+  useEffect(() => {
+    Animated.timing(fadeAnimation, {
+      toValue: 1,
+      duration: 800,
+      delay,
+      useNativeDriver: true,
+    }).start();
+  }, []);
+
+  return(
+    <Animated.View style={[
+      styles.circle, {
+        top: height * top,
+        left: width * left,
+        width: diameter,
+        height: diameter,
+        borderRadius: diameter / 2,
+        opacity: fadeAnimation,
+      },
+    ]}
+                   >
+      <View style={[styles.circlePlaceholder, { borderRadius: diameter/ 2}]} />
+    </Animated.View>
+  );
+}
