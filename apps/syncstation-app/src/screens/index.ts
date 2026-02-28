f93a0a3136547a0461231f2f045f3c443fcbc5da
nouran109 <159073597+nouran109@users.noreply.github.com>
2026-02-28 17:18:47 +0100

    fix: remove old ProfileScreen file and update export path in screens index

--- apps/syncstation-app/src/screens/index.ts
+++ apps/syncstation-app/src/screens/index.ts
@@ -1,3 +1,3 @@
 export { HomeScreen } from './HomeScreen';
 export { SelectContextScreen } from './SelectContextScreen';
-export { ProfileScreen } from './ProfileScreen';
+export { ProfileScreen } from './profile-screen';
