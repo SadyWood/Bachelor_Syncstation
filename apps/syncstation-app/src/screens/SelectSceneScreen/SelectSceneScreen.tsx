4fa35fcdaf064ab87ed0aa9b7142f17bfaf50306
nouran109 <159073597+nouran109@users.noreply.github.com>
2026-03-02 20:30:41 +0100

    feat: addd base layout and imports

---
+++ apps/syncstation-app/src/screens/SelectSceneScreen/SelectSceneScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export function SelectSceneScreen() {
+    return (
+        <View>
+            <Text>Select Scene</Text>
+        </View>
+    );
+}
